a64913c389fe3e03796eefd9251394685f94f51b
Amos Wenger <amos@bearcove.net>
2024-01-17 00:08:57 +0100

    Troubleshoot all the things

--- troubleshoot-ftl.sh
+++ troubleshoot-ftl.sh
@@ -4,6 +4,7 @@ set -u
 echo "fasterthanli.me resolves to: "
 dig fasterthanli.me +short
 
+IFS=$'\n' read -r -d '' -a nodes < <(dig +short +trace fasterthanli.me CNAME | grep CNAME | cut -d ' ' -f 2 | sed 's/[.]$//g')
 read -d '\n' curl_format << EOF
         time_connect:  %{time_connect}s
      time_appconnect:  %{time_appconnect}s
@@ -13,23 +14,24 @@ read -d '\n' curl_format << EOF
           time_total:  %{time_total}s
 EOF
 
-HOSTS=(pari falk ginz szaw hell flam marl safo pore)
-for HOST in ${HOSTS[@]}; do
+for node in ${nodes[@]}; do
     for ip in -4 -6; do
-        echo; echo "============== $HOST $ip =============="  
-        # run ping6 if $ip is -6
+        echo; echo "============== $node $ip =============="  
         if [ $ip == -6 ]; then
-            ping6 -c 3 $HOST.bearcove.cloud
+            ping6 -c 1 $node
         else
-            ping -c 3 $HOST.bearcove.cloud
+            ping -c 1 $node
         fi
         curl $ip \
             --write-out "${curl_format}" \
+            --silent \
             --output /dev/null \
             --fail \
-            --connect-timeout 5 \
-            --connect-to fasterthanli.me:443:$HOST.bearcove.cloud:443 \
+            --connect-timeout 3 \
+            --connect-to fasterthanli.me:443:$node:443 \
             "https://fasterthanli.me"
-        echo "curl exit code: $?"
+        curl_exit_code=$?
+        # if curl exit code isn't 0 print something in red
+        [ $curl_exit_code -ne 0 ] && printf "\e[31mCurl failed\e[0m\n"
     done
 done
